538b59fcb9d3baa27e11ead95d3c043980e04563
DennsHartstock <dennis.hartstock@web.de>
2023-03-31 09:15:56 +0200

    using card

--- app/src/main/java/com/commcode/learningcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/commcode/learningcompose/ui/theme/InstagramProfileCard.kt
@@ -1,8 +1,11 @@
 package com.commcode.learningcompose.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,24 +16,30 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        backgroundColor = Color.White,
+        border = BorderStroke(1.dp, color = Color.Black)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Green)
-                .border(2.dp, color = Color.Gray)
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Green)
+                    .border(2.dp, color = Color.Gray)
+            ) {
 
+            }
+            Column()
+            Column()
+            Column()
         }
-        Column()
-        Column()
-        Column()
     }
 }
 
