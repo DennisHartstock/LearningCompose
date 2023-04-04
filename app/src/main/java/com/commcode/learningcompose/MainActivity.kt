a6f434176fde3fd39b71fe9c579850b5dbc5f8f6
DennisHartstock <74893071+DennisHartstock@users.noreply.github.com>
2023-04-04 17:54:25 +0200

    add preview

--- app/src/main/java/com/commcode/learningcompose/MainActivity.kt
+++ app/src/main/java/com/commcode/learningcompose/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(MaterialTheme.colors.background)
                 ) {
-                    ComposeMaterial()
+                    Preview()
                 }
             }
         }
