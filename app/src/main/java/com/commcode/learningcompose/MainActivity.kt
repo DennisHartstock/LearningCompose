08122bede6bd5d0a734cf0aa6f384ba9d34514eb
DennsHartstock <dennis.hartstock@web.de>
2023-04-27 10:36:31 +0200

    test ActivityResultApi

--- app/src/main/java/com/commcode/learningcompose/MainActivity.kt
+++ app/src/main/java/com/commcode/learningcompose/MainActivity.kt
@@ -1,161 +1,148 @@
 package com.commcode.learningcompose
 
+//import com.commcode.learningcompose.ui.theme.InstagramProfileCard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
-import com.commcode.learningcompose.ui.theme.InstagramProfileCard
-import com.commcode.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+//        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         setContent {
-            PreviewTest(viewModel)
+            ActivityResultTest()
+//            PreviewTest(viewModel)
         }
     }
 }
-
-@Composable
-private fun PreviewTest(viewModel: MainViewModel) {
-LearningComposeTheme {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-            ) {
-                val models = viewModel.models.observeAsState(listOf())
-                LazyColumn {
-                    items(models.value, keys = {it.id}) {model ->
-                        val dismissState = rememberDismissState()
-
-                        if(dismissState.isDismissed(DismissDirection.EndToStart)) {
-                            viewModel.delete(model)
-                        }
-
-                        SwipeToDismiss(
-                            state = dismissState,
-                            directions = listOf(DismissDirection.EndToStart),
-                            background = {
-                                Box(
-                                    modifier = Modifier
-                                    .padding(16.dp)
-                                    .fillMaxSize
-                                    .background = Color.Red.copy(alpha = 0.5f),
-                                    contentAlignment = Alignment.CenterEnd
-                                ) {
-                                    Text(
-                                        modifier = Modifier.padding(16.dp),
-                                        text = "Delete item",
-                                        color = Color.White,
-                                        fontSize = 24.sp
-                                    )
-                                }
-                             }
-                        ) {
-                            InstagramProfileCard(
-                            model = model,
-                            onFollowedButtonClickListener = {
-                                viewmodel.changeFollowingStatus(it)
-                            }
-                        )
-                        }
-                    }
-                }
-        }
-     }
-}
-
-@Preview
-@Composable
-private fun Preview() {
-
-    val snackbarHostState = remember {
-        SnackbarHostState()
-    }
-    Log.d("Preview", snackbarHostState.currentSnackbarData.toString)
-    val scope = rememberCoroutineScope()
-    val fabIsVisible = remember {
-        mutableStateOf(true)
-    }
-
-Scaffold(
-    snackbarHost = {
-        SnackbarHost(hostState = snackbarHostState)
-    },
-    floatingActionButton = {
-        if (fabIsVisible.value) {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    val action = snackbarHostState.showSnackbar(
-                        message = "This is snackbar",
-                        actionLabel = "Hide Fab",
-                        duration = SnackbarDuration.Long
-                    )
-                    if (action == SnackbarResult.ActionPerformed) {
-                        fabIsVisible.value = false
-                    }
-                }
-            }) {
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = null
-                )
-            }
-        }
-    },
-    topBar = {
-        TopAppBar(title = {
-            Text(text = "TopAppBar title")
-        },
-            navigationIcon = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        Icons.Rounded.Menu,
-                        contentDescription = null
-                    )
-                }
-            })
-    },
-    drawerContent = {
-        Text(text = "1")
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = "2")
-    },
-    bottomBar = {
-        BottomNavigation() {
-            BottomNavigationItem(
-                icon = {
-                    Icon(
-                        Icons.Rounded.Lock,
-                        contentDescription = null
-                    )
-                },
-                label = { Text(text = "Locked") },
-                selected = true,
-                onClick = {}
-            )
-        }
-    }
-) {
-    Text(
-        modifier = Modifier.padding(it),
-        text = "Scaffold content"
-    )
-}
-}
-
+//
+//@Composable
+//private fun PreviewTest(viewModel: MainViewModel) {
+//LearningComposeTheme {
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(MaterialTheme.colors.background)
+//            ) {
+//                val models = viewModel.models.observeAsState(listOf())
+//                LazyColumn {
+//                    items(models.value, keys = {it.id}) {model ->
+//                        val dismissState = rememberDismissState()
+//
+//                        if(dismissState.isDismissed(DismissDirection.EndToStart)) {
+//                            viewModel.delete(model)
+//                        }
+//
+//                        SwipeToDismiss(
+//                            state = dismissState,
+//                            directions = listOf(DismissDirection.EndToStart),
+//                            background = {
+//                                Box(
+//                                    modifier = Modifier
+//                                    .padding(16.dp)
+//                                    .fillMaxSize
+//                                    .background = Color.Red.copy(alpha = 0.5f),
+//                                    contentAlignment = Alignment.CenterEnd
+//                                ) {
+//                                    Text(
+//                                        modifier = Modifier.padding(16.dp),
+//                                        text = "Delete item",
+//                                        color = Color.White,
+//                                        fontSize = 24.sp
+//                                    )
+//                                }
+//                             }
+//                        ) {
+//                            InstagramProfileCard(
+//                            model = model,
+//                            onFollowedButtonClickListener = {
+//                                viewmodel.changeFollowingStatus(it)
+//                            }
+//                        )
+//                        }
+//                    }
+//                }
+//        }
+//     }
+//}
+//
+//@Preview
+//@Composable
+//private fun Preview() {
+//
+//    val snackbarHostState = remember {
+//        SnackbarHostState()
+//    }
+//    Log.d("Preview", snackbarHostState.currentSnackbarData.toString)
+//    val scope = rememberCoroutineScope()
+//    val fabIsVisible = remember {
+//        mutableStateOf(true)
+//    }
+//
+//Scaffold(
+//    snackbarHost = {
+//        SnackbarHost(hostState = snackbarHostState)
+//    },
+//    floatingActionButton = {
+//        if (fabIsVisible.value) {
+//            FloatingActionButton(onClick = {
+//                scope.launch {
+//                    val action = snackbarHostState.showSnackbar(
+//                        message = "This is snackbar",
+//                        actionLabel = "Hide Fab",
+//                        duration = SnackbarDuration.Long
+//                    )
+//                    if (action == SnackbarResult.ActionPerformed) {
+//                        fabIsVisible.value = false
+//                    }
+//                }
+//            }) {
+//                Icon(
+//                    imageVector = Icons.Filled.Add,
+//                    contentDescription = null
+//                )
+//            }
+//        }
+//    },
+//    topBar = {
+//        TopAppBar(title = {
+//            Text(text = "TopAppBar title")
+//        },
+//            navigationIcon = {
+//                IconButton(onClick = {}) {
+//                    Icon(
+//                        Icons.Rounded.Menu,
+//                        contentDescription = null
+//                    )
+//                }
+//            })
+//    },
+//    drawerContent = {
+//        Text(text = "1")
+//        Spacer(modifier = Modifier.height(4.dp))
+//        Text(text = "2")
+//    },
+//    bottomBar = {
+//        BottomNavigation() {
+//            BottomNavigationItem(
+//                icon = {
+//                    Icon(
+//                        Icons.Rounded.Lock,
+//                        contentDescription = null
+//                    )
+//                },
+//                label = { Text(text = "Locked") },
+//                selected = true,
+//                onClick = {}
+//            )
+//        }
+//    }
+//) {
+//    Text(
+//        modifier = Modifier.padding(it),
+//        text = "Scaffold content"
+//    )
+//}
+//}
+//
--- app/src/main/java/com/commcode/learningcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/commcode/learningcompose/ui/theme/InstagramProfileCard.kt
@@ -1,132 +1,132 @@
-package com.commcode.learningcompose.ui.theme
-
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.commcode.learningcompose.R
-
-@Composable
-fun InstagramProfileCard(
-    model: InstagramModel,
-    onFollowedButtonClickListener: () -> Unit
-) {
-    Card(
-        modifier = Modifier.padding(8.dp),
-        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
-        backgroundColor = MaterialTheme.colors.background,
-        border = BorderStroke(1.dp, color = MaterialTheme.colors.onBackground)
-    ) {
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    modifier = Modifier
-                        .size(60.dp)
-                        .clip(CircleShape)
-                        .background(Color.White)
-                        .padding(8.dp),
-                    painter = painterResource(id = R.drawable.ic_instagram),
-                    contentDescription = "Instagram Logo",
-                    contentScale = ContentScale.Crop
-                )
-                UserStatistics(value = "4.956", title = "Posts")
-                UserStatistics(value = "45M", title = "Followers")
-                UserStatistics(value = "25", title = "Following")
-            }
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = "Instagram ${model.id}", fontSize = 30.sp,
-                    fontFamily = FontFamily.Cursive
-                )
-                Text(text = "#${model.title}")
-                Text(text = "Homepage")
-                FollowButton(isFollowed = model.isFollowed) {
-                    onFollowedButtonClickListener(model)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun FollowButton(
-    isFollowed: Boolean,
-    clickListener: () -> Unit
-) {
-    Button(
-        onClick = { clickListener() },
-    colors = ButtonDefaults.buttonColors (
-        backgroundColor = if(isFollowed) {
-            MaterialTheme.colors.primary.copy(alpha = 0.5f)
-        } else {
-            MaterialTheme.colors.primary
-        }
-    )
-    ) {
-        val text = if(isFollowed) {
-            "Unfollow"
-        } else {
-            "Follow"
-        }
-        Text(text = text)
-    }
-}
-
-@Composable
-private fun UserStatistics(value: String, title: String) {
-    Column(
-        modifier = Modifier
-            .height(80.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Text(
-            modifier = Modifier.padding(start = 4.dp, end = 4.dp),
-            text = value, fontSize = 24.sp, fontFamily = FontFamily.Cursive
-        )
-        Text(
-            modifier = Modifier.padding(start = 4.dp, end = 4.dp),
-            text = title, fontWeight = FontWeight.Bold
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewCardLight() {
-    LearningComposeTheme(darkTheme = false) {
-        InstagramProfileCard()
-    }
-}
-
-@Preview
-@Composable
-fun PreviewCardDark() {
-    LearningComposeTheme(darkTheme = true) {
-        InstagramProfileCard()
-    }
-}
+//package com.commcode.learningcompose.ui.theme
+//
+//import androidx.compose.foundation.BorderStroke
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.shape.CircleShape
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material.Button
+//import androidx.compose.material.Card
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Text
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.clip
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.layout.ContentScale
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.text.font.FontFamily
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import com.commcode.learningcompose.R
+//
+//@Composable
+//fun InstagramProfileCard(
+//    model: InstagramModel,
+//    onFollowedButtonClickListener: () -> Unit
+//) {
+//    Card(
+//        modifier = Modifier.padding(8.dp),
+//        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+//        backgroundColor = MaterialTheme.colors.background,
+//        border = BorderStroke(1.dp, color = MaterialTheme.colors.onBackground)
+//    ) {
+//        Column {
+//            Row(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(8.dp),
+//                horizontalArrangement = Arrangement.SpaceEvenly,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Image(
+//                    modifier = Modifier
+//                        .size(60.dp)
+//                        .clip(CircleShape)
+//                        .background(Color.White)
+//                        .padding(8.dp),
+//                    painter = painterResource(id = R.drawable.ic_instagram),
+//                    contentDescription = "Instagram Logo",
+//                    contentScale = ContentScale.Crop
+//                )
+//                UserStatistics(value = "4.956", title = "Posts")
+//                UserStatistics(value = "45M", title = "Followers")
+//                UserStatistics(value = "25", title = "Following")
+//            }
+//            Column(modifier = Modifier.padding(16.dp)) {
+//                Text(
+//                    text = "Instagram ${model.id}", fontSize = 30.sp,
+//                    fontFamily = FontFamily.Cursive
+//                )
+//                Text(text = "#${model.title}")
+//                Text(text = "Homepage")
+//                FollowButton(isFollowed = model.isFollowed) {
+//                    onFollowedButtonClickListener(model)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//private fun FollowButton(
+//    isFollowed: Boolean,
+//    clickListener: () -> Unit
+//) {
+//    Button(
+//        onClick = { clickListener() },
+//    colors = ButtonDefaults.buttonColors (
+//        backgroundColor = if(isFollowed) {
+//            MaterialTheme.colors.primary.copy(alpha = 0.5f)
+//        } else {
+//            MaterialTheme.colors.primary
+//        }
+//    )
+//    ) {
+//        val text = if(isFollowed) {
+//            "Unfollow"
+//        } else {
+//            "Follow"
+//        }
+//        Text(text = text)
+//    }
+//}
+//
+//@Composable
+//private fun UserStatistics(value: String, title: String) {
+//    Column(
+//        modifier = Modifier
+//            .height(80.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.SpaceEvenly
+//    ) {
+//        Text(
+//            modifier = Modifier.padding(start = 4.dp, end = 4.dp),
+//            text = value, fontSize = 24.sp, fontFamily = FontFamily.Cursive
+//        )
+//        Text(
+//            modifier = Modifier.padding(start = 4.dp, end = 4.dp),
+//            text = title, fontWeight = FontWeight.Bold
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewCardLight() {
+//    LearningComposeTheme(darkTheme = false) {
+//        InstagramProfileCard()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewCardDark() {
+//    LearningComposeTheme(darkTheme = true) {
+//        InstagramProfileCard()
+//    }
+//}
