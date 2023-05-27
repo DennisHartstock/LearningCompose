fcda538a922a5deee301b736c8b837b7a1bf7951
DennsHartstock <dennis.hartstock@web.de>
2023-05-27 08:43:25 +0200

    Animation Spec

--- app/src/main/java/com/commcode/learningcompose/ui/screen/Animation.kt
+++ app/src/main/java/com/commcode/learningcompose/ui/screen/Animation.kt
@@ -1,9 +1,15 @@
 package com.commcode.learningcompose.ui.screen
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -26,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -45,7 +52,24 @@ fun Animation() {
             mutableStateOf(true)
         }
 
-        val size by animateDpAsState(targetValue = if (isIncreased) 200.dp else 100.dp)
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val size by infiniteTransition.animateFloat(
+            initialValue = 200f,
+            targetValue = 100f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 2000),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val rotation by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 2000, easing = LinearEasing)
+            )
+        )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -57,8 +81,9 @@ fun Animation() {
         }
         AnimatedContainer(
             text = "Size",
-            size = size
+            size = size.dp
         )
+
         var isRect by remember {
             mutableStateOf(true)
         }
@@ -75,7 +100,8 @@ fun Animation() {
         }
         AnimatedContainer(
             text = "Shape",
-            radiusPercent = radius
+            radiusPercent = radius,
+            rotation = rotation
         )
 
         var isSelected by remember {
@@ -145,6 +171,7 @@ private fun AnimatedContainer(
     borderWidth: Dp = 0.dp,
     backgroundColor: Color = Color.Blue,
     alpha: Float = 1f,
+    rotation: Float = 0f,
 ) {
     Box(
         modifier = Modifier
@@ -152,7 +179,8 @@ private fun AnimatedContainer(
             .clip(RoundedCornerShape(radiusPercent))
             .border(borderWidth, Color.DarkGray)
             .background(backgroundColor)
-            .alpha(alpha),
+            .alpha(alpha)
+            .rotate(rotation),
         contentAlignment = Alignment.Center
     ) {
         Text(
